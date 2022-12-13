e41825aecba53b797e16a7eee1b9ef765e92931d
Staluks <janchukssss@inbox.lv>
2022-12-13 15:14:34 +0200

    ammended task CoordinatePane

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,19 +2,23 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
-        boolean xPositive = x > 0;
-        boolean yPositive = y > 0;
-        if (xPositive) {
-            if (yPositive) {
-                System.out.println("first");
-            } else {
-                System.out.println("second");
-            }
+        if(x==0 & y==0){
+            System.out.println("zero");
         }else {
-            if (yPositive) {
-                System.out.println("fourth");
+            boolean xPositive = x > 0;
+            boolean yPositive = y > 0;
+            if (xPositive) {
+                if (yPositive) {
+                    System.out.println("first");
+                } else {
+                    System.out.println("fourth");
+                }
             } else {
-                System.out.println("third");
+                if (yPositive) {
+                    System.out.println("second");
+                } else {
+                    System.out.println("third");
+                }
             }
         }
     }
